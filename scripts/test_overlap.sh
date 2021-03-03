fdb062c3d576e3019e9e010c79e189fac6b5843f
Colin Sullivan <colin@synadia.com>
2021-03-03 09:03:37 -0700

    rename/refactor
    Signed-off-by: Colin Sullivan <colin@synadia.com>

--- scripts/test_overlap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo { \"name\": \"strm_a\", \"subjects\": [ \"foo\" ], \"retention\": \"limits\", \"max_consumers\": -1, \"max_msgs\": 1000000, \"max_bytes\": -1, \"max_age\": 0, \"max_msg_size\": -1, \"storage\": \"file\", \"discard\": \"old\", \"num_replicas\": 2 } > tmp.json
-
-natscli stream create --config=tmp.json > oo
-
-echo { \"name\": \"strm_b\", \"subjects\": [ \"foo\" ], \"retention\": \"limits\", \"max_consumers\": -1, \"max_msgs\": 1000000, \"max_bytes\": -1, \"max_age\": 0, \"max_msg_size\": -1, \"storage\": \"file\", \"discard\": \"old\", \"num_replicas\": 2 } > tmp.json
-
-natscli stream create --config=tmp.json > oo
