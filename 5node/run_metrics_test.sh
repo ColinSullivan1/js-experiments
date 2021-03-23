dd6758a1af69b9ebdeed7599c7c62897d4f155dd
Colin Sullivan <colin@synadia.com>
2021-03-23 15:55:25 -0600

    updates
    Signed-off-by: Colin Sullivan <colin@synadia.com>

--- 5node/run_metrics_test.sh
+++ 5node/run_metrics_test.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 
 SaveMetric() {
-    echo `date +"%T"` $1 >> $3
-    echo "server_name, server_id, mem, in_msgs, out_msgs, in_bytes, out_bytes, cpu, js_storage" >> $3
-    curl -s $2/varz cat | jq -r '[.server_name, .server_id, .mem, .in_msgs, .out_msgs, .in_bytes, .out_bytes, .cpu, .jetstream.stats.storage] |  @csv'  >> $3
-    curl -s $2/routez   | jq -r '(.routes[0] | keys_unsorted), (.routes[] | to_entries | map(.value))|@csv' >> $3
-    echo ""
+    sdate=`date +"%T"`
+    prefix="$1,`date +"%T"`"
+    echo ${prefix}
+    echo "server_name, server_id, mem, in_msgs, out_msgs, in_bytes, out_bytes, cpu, js_storage" > metrics/$3.csv
+    curl -s $2/varz cat | jq -r '[.server_name, .server_id, .mem, .in_msgs, .out_msgs, .in_bytes, .out_bytes, .cpu, .jetstream.stats.storage] |  @csv'  >> metrics/$3.csv
+    curl -s $2/routez   | jq -r '(.routes[0] | keys_unsorted), (.routes[] | to_entries | map(.value))|@csv' >> metrics/$3.csv
+    sed 's/^/'"$prefix"',/' metrics/$3.csv >> metrics/$3.complete.csv
 }
 
 GetMetrics() {
-   SaveMetric $1 "localhost:8221" metrics/s1.metrics
-   SaveMetric $1 "localhost:8222" metrics/s2.metrics
-   SaveMetric $1 "localhost:8223" metrics/s3.metrics
-   SaveMetric $1 "localhost:8224" metrics/s4.metrics
-   SaveMetric $1 "localhost:8225" metrics/s5.metrics
+   SaveMetric $1 "localhost:8221" s1
+   SaveMetric $1 "localhost:8222" s2
+   SaveMetric $1 "localhost:8223" s3
+   SaveMetric $1 "localhost:8224" s4
+   SaveMetric $1 "localhost:8225" s5
 }
 
 ../scripts/kill_servers.sh
@@ -37,8 +39,9 @@ nats stream create --config=stream.json foostream
 sleep 60
 GetMetrics "1_Minute_One_Stream" 
 
-GetMetrics "Before_Publishing"
-nats request foo "{{Random 1024 1024}}" --count 10000
+GetMetrics "Before_Publishing_100MB"
+nats request foo "{{Random 1024 1024}}" --count 100000
+sleep 20
 GetMetrics "After_Publishing"
 
 GetMetrics "Before_Consumer_Creation"
@@ -48,6 +51,6 @@ GetMetrics "1_Minute_One_Stream_and_One_Consumer"
 
 GetMetrics "Before_Consumption"
 simplesub/sub.sh
-sleep 1
+sleep 10
 GetMetrics "After_Consumption"
 
--- 5node/simplesub/sub.sh
+++ 5node/simplesub/sub.sh
@@ -2,5 +2,5 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 export CLASSPATH=$DIR/jars/jnats-2.9.0.jar:$DIR
-java -cp $CLASSPATH NatsJsSimpleSub "localhost:4222" foo 1000 cons1
+java -cp $CLASSPATH NatsJsSimpleSub "localhost:4222" foo 100000 cons1
 
